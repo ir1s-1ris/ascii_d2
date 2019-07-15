91fe1e64f4a193b7a951092d6148d30a4c2cee51
ir1s-1ris <ddvnyy@yandex.ru>
2019-07-15 16:53:07 +0300

    Bot can return ID last played match

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-##api = d2api.APIWrapper('2BF19D0B6FA5E22C8364B7237B313F1B')
-
 # Fetch last 100 very high skill games and filter out games that have leavers
 import d2api
 
 api = d2api.APIWrapper('2BF19D0B6FA5E22C8364B7237B313F1B')
-vhs = api.get_match_history(skill = 3)
+vhs = api.get_match_history(account_id = 78312288)
 
-matches = [api.get_match_details(m['match_id']) for m in vhs['matches']]
+#matches = [api.get_match_details(m['match_id']) for m in vhs['matches']]
+print(type(vhs))
+print(type(vhs['matches']))
+listMatches = vhs['matches'][0]
 
-# now filter out matches that have leavers
-matches = [m for m in matches if not m.has_leavers()]
-
-# number of matches that remain
-print(len(matches))
-
-# print the first match
-print(matches[0])
+print(type(listMatches))
+print(listMatches['match_id'])
