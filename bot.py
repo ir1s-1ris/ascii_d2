56adda1dcac533719b63e5ef7288cd631f928192
ir1s-1ris <ddvnyy@yandex.ru>
2019-07-15 06:49:21 +0300

    initialisation commit

---
+++ bot.py
@@ -0,0 +1,13 @@
+import telebot
+
+bot = telebot.TeleBot("894268478:AAEkmO_QCIvNgmUGn-aReWFLSCxdyK-uvbI")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling()
