038760c92fe49289cff340e6a3041c7c9744c4bc
ir1s-1ris <w4ve.dark@yandex.ru>
2019-09-06 03:48:03 +0300

    1

--- AsciiTest2.py
+++ AsciiTest2.py
@@ -7,6 +7,7 @@ from asciimatics.renderers import FigletText
 
 import sys
 import getpass
+import sqlite3
 
 class ListView(Frame):
     def __init__(self, screen):
@@ -37,12 +38,42 @@ class ListView(Frame):
     def _quit():
         raise StopApplication("User pressed quit")
 
+class myUser():
+    def __init__(self):
+        self.name = getpass.getuser()
+
+class myDb(object):
+    def __init__(self):
+
+        self._db = sqlite3.connect('asciiDb.db')
+        self._db.row_factory = sqlite3.Row
+
+        self._db.cursor().execute('''
+            CREATE TABLE if not exists users(
+                id INTEGER PRIMARY KEY,
+                username TEXT,
+                regdate INT,
+                steamId INT
+            )
+        ''')
+        self._db.commit()
+    
+    def write(self, sql):
+        self._db.cursor().execute(sql)
+        self._db.commit()
+
+    def read(self, sql):
+        self._db.cursor().execute(sql).fetchall()
+
+
 #возвращает эффект
 class HelloScene(Print): 
     def __init__(self, screen):
+        activeUser = myUser()
+        text = "Hello " + str(activeUser.name)
         super(HelloScene, self).__init__(
             screen,
-            FigletText("Class Rabotaet!", "big"),
+            FigletText(text, "big"),
             screen.height // 3,
             screen.width // 3,
             colour = screen.COLOUR_GREEN,
@@ -50,14 +81,36 @@ class HelloScene(Print):
             speed = 1
         )
 
+class frame1(Frame):
+    def __init__(self, screen, activeDb):
+        super(frame1, self).__init__(
+            screen,
+            int(screen.height * 0.99),
+            int(screen.width * 0.99),
+            on_load = self._reload_list,
+            hover_focus=True,
+            can_scroll=False,
+            title="frame1"
+        )
+        self._activeDb = activeDb
 
-class myUser():
-    def __init__(self):
-        self.name = getpass.getuser()
+        layout = Layout([100], fill_frame = True)
+        self.add_layout(layout)
+        
+
+    def _reload_list(self):
+        print('bak')
+
+    
+
+
+    
+
 
 
 
 def demo(screen, scene):
+
     scenes = [
         Scene([ListView(screen)], -1, name="Main"),
         Scene([HelloScene(screen)], -1, name="HelloScene")
@@ -66,24 +119,14 @@ def demo(screen, scene):
     screen.play(scenes, stop_on_resize=True, start_scene=scene, allow_int=True)
 
 
-
+activeDb = myDb()
 last_scene = None
-# while True:
-#     try:
-#         Screen.wrapper(demo, catch_interrupt=True, arguments=[last_scene])
-#         sys.exit(0)
-#     except ResizeScreenError as e:
-#         last_scene = e.scene
-
-user1 = myUser()
-
-# print(type(user1))
-print(user1.name)
-
-
-
-
-
+while True:
+    try:
+        Screen.wrapper(demo, catch_interrupt=True, arguments=[last_scene])
+        sys.exit(0)
+    except ResizeScreenError as e:
+        last_scene = e.scene
 
 
 
